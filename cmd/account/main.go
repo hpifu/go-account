70e53c76e703f52f00b4cfc6b13f1631d0a9ff32
hatlonely <hatlonely@gmail.com>
2019-08-29 18:30:39 +0800

    commit message

--- cmd/account/main.go
+++ cmd/account/main.go
@@ -87,7 +87,7 @@ func main() {
 	r := gin.New()
 	r.Use(gin.Recovery())
 	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:4000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://account.hatlonely.com")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
